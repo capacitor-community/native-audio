9b1dec2037e5096d36ac2be50f16e8b1b817e7b4
Roman <36918149+akhromieiev@users.noreply.github.com>
2021-11-26 23:18:19 +0200

    Add pause method
    The pause method is missed

--- src/definitions.ts
+++ src/definitions.ts
@@ -2,6 +2,7 @@ export interface NativeAudio {
   configure(options: ConfigureOptions): Promise<void>;
   preload(options: PreloadOptions): Promise<void>;
   play(options: { assetId: string, time: number }): Promise<void>;
+  pause(options: { assetId: string }): Promise<void>;
   resume(options: { assetId: string }): Promise<void>;
   loop(options: { assetId: string }): Promise<void>;
   stop(options: { assetId: string }): Promise<void>;
