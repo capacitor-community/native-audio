2d176283d61b58f21748555f4307d52253277492
Kyoz <banminkyoz@gmail.com>
2022-07-25 23:48:50 +0700

    fix(ios): resolve already exist asset

--- ios/Plugin/Constant.swift
+++ ios/Plugin/Constant.swift
@@ -16,4 +16,5 @@ public class Constant {
     public static let ErrorAssetId = "Asset Id is missing"
     public static let ErrorAssetPath = "Asset Path is missing"
     public static let ErrorAssetNotFound = "Asset is not loaded"
+    public static let ErrorAssetExists = "Audio Asset already exists"
 }
